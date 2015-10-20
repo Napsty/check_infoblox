51025e41530b773db0967b084e86acef6687365c
Claudio Kuenzler <ck@claudiokuenzler.com>
2015-10-20 11:31:45 +0200

    Added several checks

--- check_infoblox.sh
+++ check_infoblox.sh
@@ -4,9 +4,10 @@
 # Author:   Claudio Kuenzler www.claudiokuenzler.com                       #
 # Purpose:  Monitor Infoblox Appliance                                     #
 # License:  GPLv2                                                          #
+# Docs:     www.claudiokuenzler.com/nagios-plugins/check_infoblox.php      #
 # History:                                                                 #
 # 20151016  Started Script programming. Check: cpu, mem                    #
-# 20151020  Added checks: replication                                      #
+# 20151020  Added check: replication, grid, info, ip                       #
 ############################################################################
 # Variable Declaration
 STATE_OK=0              # define the exit code if status is OK
@@ -28,14 +29,25 @@ Options:
 -V SNMP Version to use (currently only 2c is supported)
 -C SNMP Community (default: public)
 -t Type to check 
--d Domain name (for check type 'dnsstats')
+-a Additional arguments for certain checks
 -w Warning Threshold (optional)
 -c Critical Threshold (optional)
 -h This help text
 
 Check Types:
 ------------
-cpu -> Check CPU utilization
+cpu -> Check CPU utilization (thresholds possible)
+mem -> Check Memory utilization (thresholds possible)
+replication -> Check if replication between Infoblox appliances is working
+grid -> Check if appliance is Active or Passive in grid (additional argument possible)
+info -> Display general information about this appliance
+ip -> Display configured ip addresses of this appliance (additional argument possible to check for a certain address)
+
+Additional Arguments:
+------------
+example.com (domain name) for dnsstat check
+(Active|Passive) for grid check
+ip.add.re.ss for ip check
 "
 exit ${STATE_UNKNOWN} 
 }
@@ -44,14 +56,14 @@ exit ${STATE_UNKNOWN}
 if [[ "$1" = "--help" ]] || [[ ${#} -eq 0 ]]; then help; fi
 
 # Get Opts
-while getopts "H:V:C:t:d:w:c:h" Input;
+while getopts "H:V:C:t:a:w:c:h" Input;
 do
   case ${Input} in
     H) host=${OPTARG};;
     V) snmpv=${OPTARG};;
     C) snmpc=${OPTARG};;
     t) checktype=${OPTARG};;
-    d) domain=${OPTARG};;
+    a) addarg=${OPTARG};;
     w) warning=${OPTARG};;
     c) critical=${OPTARG};;
     h) help;;
@@ -90,6 +102,31 @@ if [[ $? -gt 0 ]]; then echo "UNKNOWN - SNMP connection failed"; exit ${STATE_UN
 ############################################################################
 # Check Types
 case ${checktype} in
+
+info) # Displays information about this Infoblox appliance
+  model=$(snmpwalk -Oqv -v ${snmpv} -c ${snmpc} ${host} 1.3.6.1.4.1.7779.3.1.1.2.1.4.0)
+  hwn=$(snmpwalk -Oqv -v ${snmpv} -c ${snmpc} ${host} 1.3.6.1.4.1.7779.3.1.1.2.1.5.0)
+  systemsn=$(snmpwalk -Oqv -v ${snmpv} -c ${snmpc} ${host} 1.3.6.1.4.1.7779.3.1.1.2.1.6.0)
+  softv=$(snmpwalk -Oqv -v ${snmpv} -c ${snmpc} ${host} 1.3.6.1.4.1.7779.3.1.1.2.1.7.0)
+  echo "System Name: $systemname, Infoblox Model: $model, HW ID: $hwn, SN: $systemsn, Software Version: $softv"
+  exit ${STATE_OK}
+;;
+
+ip) # Display configured ip addresses of this appliance
+  ipaddrs=($(snmpwalk -Oqv -v ${snmpv} -c ${snmpc} ${host} 1.3.6.1.2.1.4.20.1.1))
+  if [[ -n $addarg ]]; then 
+    if [[ -z $(echo ${ipaddrs[*]} | grep $addarg) ]]; then
+      echo "IP WARNING: Expected address $addarg not found. IP addresses configured: ${ipaddrs[*]}" 
+      exit ${STATE_WARNING}
+    else 
+      echo "IP OK: Addresses: ${ipaddrs[*]}"
+      exit ${STATE_OK} 
+    fi 
+  else
+    echo "IP OK - Addresses: ${ipaddrs[*]}"
+  fi
+  exit ${STATE_OK}
+;;
 
 cpu) # Checks the cpu utilization in percentage
   usage=$(snmpwalk -v ${snmpv} -c ${snmpc} -Oqv ${host} 1.3.6.1.4.1.7779.3.1.1.2.1.8.1)
@@ -165,6 +202,28 @@ replication) # Check the replication between Infoblox master/slave appliances
     echo "REPLICATION UNKNOWN - This system (SN: ${systemsn}) is a passive grid member. Cannot verify replication. Try with HA IP address?"
     exit ${STATE_UNKNOWN}
   fi
+;;
 
+grid) # Check grid status
+  gridstatus=$(snmpwalk -v ${snmpv} -c ${snmpc} -Oqv ${host} 1.3.6.1.4.1.7779.3.1.1.2.1.13 | sed "s/\"//g")
+  systemsn=$(snmpwalk -Oqv -v ${snmpv} -c ${snmpc} ${host} 1.3.6.1.4.1.7779.3.1.1.2.1.6.0)
+  if [[ -n $addarg ]] && ([[ "$addarg" = "Active" ]] || [[ "$addarg" = "Passive" ]]); then
+    if [[ "$gridstatus" != "$addarg" ]]; then
+      echo "GRID STATUS WARNING - This member (SN: $systemsn) is $gridstatus but expected $addarg"
+      exit ${STATE_WARNING}
+    fi
+  elif [[ -n $addarg ]]; then 
+    echo "GRID STATUS UNKNOWN - Please use Active or Passive as additional arguments"
+    exit ${STATE_UNKNOWN}
+  else 
+    echo "GRID STATUS OK - This member (SN: $systemsn) is $gridstatus"
+    exit ${STATE_OK}
+  fi
+
+
+    
+
+  
+;;
 
 esac
